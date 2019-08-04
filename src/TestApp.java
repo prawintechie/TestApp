991fb894213da5ca3d072d2bedc1a5732c90c31a
prawintechie <learnalways999@gmail.com>
2019-08-04 08:34:23 +0530

    from master

--- src/TestApp.java
+++ src/TestApp.java
@@ -3,10 +3,9 @@ public class TestApp {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!!! ok");
+		System.out.println("some text");
 	}
 	
-	private static void get() {
-		
-	}
+	
 
 }
